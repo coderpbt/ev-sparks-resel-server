5ac2aa872d3ada8e5fefff397955cf79472351f9
coderpbt <yeasin800398@gmail.com>
2022-11-25 23:17:57 +0600

    product add

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
+// const categoris = require('./data/categories.json')
+// const courses = require('./data/coursces.json')
 
 //middle ware
 app.use(cors());
@@ -17,27 +20,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
   try {
-    const evSparksProductsCollection = client.db("fitzeosUserDb").collection("products");
+    const evSparksProductsCollection = client.db("evSparksDB").collection("categoris");
+    const evSparksProductsWiseCollection = client.db("evSparksDB").collection("productswise");
     //server to ui data
-    app.get('/products', async (req, res) => {
+    app.get('/categoris', async (req, res) => {
       const query = {}
       const cursor = evSparksProductsCollection.find(query);
       const result = await cursor.toArray()
-      // const result = results
       res.send(result)
     })
+
+    app.get('/categoris/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { category_id: id }
+      const booking = await evSparksProductsWiseCollection.find(query).toArray()
+      res.send(booking)
+    })
+
+
+
+    //server to ui data
+    app.get('/productswise', async (req, res) => {
+      const query = {}
+      const cursor = evSparksProductsWiseCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    // app.get('/productswise/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { category_id: id }
+    //   const booking = await evSparksProductsWiseCollection.find(query).toArray()
+    //   res.send(booking)
+    // })
+
 
   } finally {
 
   }
 }
 run().catch(err => console.log(err));
-
-
-
-
-
-
 
 
 
