1ba99c920f40f55e855ded9bd4a312e4e0a02eef
coderpbt <yeasin800398@gmail.com>
2022-11-25 23:21:00 +0600

    user collection create

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const port = process.env.PORT || 5000
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-// const categoris = require('./data/categories.json')
-// const courses = require('./data/coursces.json')
-
 //middle ware
 app.use(cors());
 app.use(express.json());
@@ -22,6 +19,9 @@ async function run() {
   try {
     const evSparksProductsCollection = client.db("evSparksDB").collection("categoris");
     const evSparksProductsWiseCollection = client.db("evSparksDB").collection("productswise");
+    const evSparksBookingCollection = client.db("evSparksDB").collection("booking");
+    const evSparksUserCollection = client.db("evSparksDB").collection("users");
+    
     //server to ui data
     app.get('/categoris', async (req, res) => {
       const query = {}
@@ -36,9 +36,6 @@ async function run() {
       const booking = await evSparksProductsWiseCollection.find(query).toArray()
       res.send(booking)
     })
-
-
-
     //server to ui data
     app.get('/productswise', async (req, res) => {
       const query = {}
@@ -46,13 +43,34 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
+    app.get('/productswise/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id : ObjectId(id) }
+      const booking = await evSparksProductsWiseCollection.find(query).toArray()
+      res.send(booking)
+    })
 
-    // app.get('/productswise/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { category_id: id }
-    //   const booking = await evSparksProductsWiseCollection.find(query).toArray()
-    //   res.send(booking)
-    // })
+    //product booking ui to db
+    app.post('/booking', async(req, res) => {
+      const booking = req.body;
+      const result = await evSparksBookingCollection.insertOne(booking)
+      res.send(result)
+    })
+
+    //email deya single user ar data bair kora
+    app.get('/bookings', async(req,res) => {
+      const email = req.query.email;
+      const query = {email : email}
+      const booking = await evSparksBookingCollection.find(query).toArray();
+      res.send(booking)
+    })
+
+    // user registion save db
+    app.post('/users', async(req,res) => {
+      const user = req.body;
+      const result = await evSparksUserCollection.insertOne(user)
+      res.send(result)
+    })
 
 
   } finally {
@@ -63,11 +81,10 @@ run().catch(err => console.log(err));
 
 
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Ev Sparks app listening on port ${port}`)
 })
