0b51666757ba89679b6b702acd7147c3cb6ae9d4
coderpbt <yeasin800398@gmail.com>
2022-11-27 23:45:25 +0600

    product delete

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ async function run() {
       res.send(result)
     })
 
+      //delete product
+      app.delete('/doctors/:id', verifyJWT, verifyAdmin, async(req,res) => {
+      const id = req.params.id
+      const query =  { _id : ObjectId(id)}
+      const doctors = await doctorsCollection.deleteOne(query)
+      res.send(doctors)
+    })
+
 
 
 
